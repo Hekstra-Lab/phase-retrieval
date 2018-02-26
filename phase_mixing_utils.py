192d267628fef89743bf8fff3450f437344b7b8a
ianhi <ianhuntisaak@gmail.com>
2018-02-25 23:44:49 -0500

    Updated phase_intensity_plot function
    - Fixed problem of anlge going from positive to negative around 180
    degrees
    - Added more options to function (e.g. using a matpltolib axis object)

--- phase_mixing_utils.py
+++ phase_mixing_utils.py
@@ -11,13 +11,10 @@ def get_mag(FT_arr):
 
 def get_phase(FT_arr):
     """
-    Gets the complex phases 
+    Gets the complex phases
     """
-    phase = np.arctan2(np.imag(FT_arr),np.real(FT_arr))
+    return np.angle(FT_arr)
 
-
-
-    return phase
 def mix_FT(mag_arr, phase_arr):
     """
     Performs the fourier transforms and then mixes the phases and magnitudes.
@@ -38,33 +35,42 @@ def mix_FT_arr(mag_arr, phase_arr):
 
     return mag*np.exp(1j*phase)
 
-def phase_intensity_plot(arr, cb=True):
+def phase_intensity_plot(arr, ax=None, cb=True, min_alpha = 0, cmap = 'ocean'):
     """
     inputs:
         arr: An array of complex numbers. This should be fftshifted already
+        ax: matplotlib axis object if you want the plot to happen in that
         cb: Do you want to show a vertical colorbar. Default True
+        min_alpha: set to map the magnitudes on to alphas = [min_alpha,1]
+        cmap: default is cmocean phase cm, otherwise the string will be used (hsv a good alternative)
     outputs:
         A plot following kevin cowtan's convention
     """
 
     r = get_mag(arr)
     theta = get_phase(arr)
+    theta[theta<0]+=np.pi #correct for neg values of angle so no weird jumps going around 180 deg
     norm = plt.Normalize()
 
     disp_arr = phase_cm(norm(theta))
     disp_arr[:,:,-1] = r/np.max(r)
-    fig, ax = plt.subplots(figsize=(10,10))
-
+    disp_arr[:,:,-1] += (1-disp_arr[:,:,-1])*min_alpha
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10,10))
+    elif ax is not None and cb:
+        raise ValueError('if you pass an axis you can\'t have cb=True')
+    if cmap == 'ocean':
+        cmap = phase_cm
     # Relabel the colorbar without rescaling theta to be in [0,2pi]
     if cb:
-        cax = ax.imshow(disp_arr,cmap=phase_cm)
+        cax = ax.imshow(np.flipud(disp_arr),cmap=cmap)
         cbar = fig.colorbar(cax,ticks=np.linspace(0,1,5))
         labels = [ r"0", r"$\frac{\pi}{2}$", r"$\pi$",
                    r"$\frac{3\pi}{2}$", r"$2\pi$"]
         cbar.ax.set_yticklabels(labels, fontsize=20)
     else:
-        ax.imshow(disp_arr, cmap='hsv')
-    plt.show()
+        ax.imshow(np.flipud(disp_arr), cmap=cmap)
+    if ax is None:
 
 
 
@@ -91,11 +97,4 @@ def gaussian_lattice(n_atoms,uc_size,n_tiles, means):
 def rings():
     th = np.linspace(0,2*np.pi,11)
     circ_mu = np.array([[50+20*np.cos(t),50+20*np.sin(t)] for t in th[:-1]])
-    return gaussian_lattice(10,100,20, circ_mu) 
-
-
-
-
-
-
-
+    return gaussian_lattice(10,100,20, circ_mu)
