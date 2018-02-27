527ef147322721827f98e797764e046d1606667c
Jack Greisman <Jack.Greisman@gmail.com>
2018-02-27 15:23:04 -0500

    Update docstrings for phase_mixing_utils.py

--- phase_mixing_utils.py
+++ phase_mixing_utils.py
@@ -1,3 +1,11 @@
+"""
+phase_mixing_utils.py
+
+Utilities for messing around with phase retrieval algorithms and 
+visualizing the phase problem
+"""
+
+from scipy.stats import multivariate_normal
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -5,46 +13,100 @@ from cmocean.cm import phase as phase_cm
 
 def get_mag(FT_arr):
     """
-    silly fxn, but nice symmetry with less trivial get_phase() function.
+    Returns the magnitudes of the intensities from a Fourier transform
+
+    Parameters
+    ----------
+    FT_arr : np.ndarray
+        An array of complex numbers
+
+    Returns
+    -------
+    np.ndarray
+        Array of magnitudes from Fourier transform
     """
     return np.abs(FT_arr)
 
 def get_phase(FT_arr):
     """
-    Gets the complex phases
+    Returns the angles of the complex phases from a Fourier transform
+
+    Parameters
+    ----------
+    FT_arr : np.ndarray
+        An array of complex numbers
+
+    Returns
+    -------
+    np.ndarray
+        Angles of the complex phases from Fourier transform
     """
     return np.angle(FT_arr)
 
 def mix_FT(mag_arr, phase_arr):
     """
-    Performs the fourier transforms and then mixes the phases and magnitudes.
-    Calls mix_FT_arr() and Returns a new complex array
-    """
+    Performs the Fourier transforms and then mixes the provided phases 
+    and magnitudes.
 
+    Parameters
+    ----------
+    mag_arr : np.ndarray
+        Array of magnitudes
+    phase_arr : np.ndarray
+        Array of complex phases
+
+    Returns
+    -------
+    np.ndarray
+        New complex array
+    """
     return mix_FT_arr(np.fft.fft2(mag_arr),np.fft.fft2(phase_arr))
 
 def mix_FT_arr(mag_arr, phase_arr):
     """
-    Mixes two fourier transform arrays by taking the magnitude from one and
-    the phase from the other. Returns a new complex array
+    Mixes two Fourier transform arrays by taking the magnitude from one 
+    and the phase from the other. Arrays must be the same shape.
+    
+    Parameters
+    ----------
+    mag_arr : np.ndarray
+        Array of magnitudes
+    phase_arr : np.ndarray
+        Array of complex phases
 
-    arrays must be the same shape.
+    Returns
+    -------
+    np.ndarray
+        New complex array
     """
     mag = get_mag(mag_arr)
     phase = get_phase(phase_arr)
 
     return mag*np.exp(1j*phase)
 
-def phase_intensity_plot(arr, ax=None, cb=True, min_alpha = 0, cmap = 'ocean'):
+def phase_intensity_plot(arr, ax=None, cb=True, min_alpha=0.0, cmap='ocean'):
     """
-    inputs:
-        arr: An array of complex numbers. This should be fftshifted already
-        ax: matplotlib axis object if you want the plot to happen in that
-        cb: Do you want to show a vertical colorbar. Default True
-        min_alpha: set to map the magnitudes on to alphas = [min_alpha,1]
-        cmap: default is cmocean phase cm, otherwise the string will be used (hsv a good alternative)
-    outputs:
-        A plot following kevin cowtan's convention
+    Plot the provided Fourier transform representing intensity using
+    alpha value and phase using colormap.
+
+    Parameters
+    ----------
+    arr : np.ndarray
+        An array of complex numbers. This should be fftshifted already.
+    ax : matplotlib axis object 
+        If provided, plot is drawn to axis object
+    cb : boolean
+        Show colorbar corresponding to phases (Default is True).
+    min_alpha : float
+        Map the magnitude of intensities to alphas: [min_alpha,1]
+        (Default is 0.0).
+    cmap : matplotlib colormap
+        Colormap for phases -- string will be used if provided. 
+        (Default is cmocean phase cm -- hsv is a good alternative).
+    
+    Returns
+    -------
+        Phase plot following Kevin Cowtan's Book of Fourier convention. 
     """
 
     r = get_mag(arr)
@@ -73,15 +135,29 @@ def phase_intensity_plot(arr, ax=None, cb=True, min_alpha = 0, cmap = 'ocean'):
     if ax is None:
 	plt.show()
 
+def gaussian_lattice(n_atoms, uc_size, n_tiles, means):
+    """
+    Create a square lattice composed of unit cells each containg n_atoms
+    gaussians of unit variance.
 
-from scipy.stats import multivariate_normal
+    Lattice dimensions are (uc_size*n_tiles)x(uc_size*n_tiles) 
 
-def gaussian_lattice(n_atoms,uc_size,n_tiles, means):
-    """ Create a square lattice composed of unit cells each containg n_atoms gaussians of unit variance
-        Lattice dimensions are (uc_size*n_tiles)x(uc_size*n_tiles)
-        Means is an array of locations for the gaussians - each value must be less than uc_size
-        Default variance is uc_size/10
-        Output is an array containg the lattice"""
+    Parameters
+    ----------
+    n_atoms : int
+        Number of atom-centered Gaussians
+    uc_size : float
+        Dimensions of unit cell
+    n_tiles : float or int
+        Number of unit cells to tile to generate lattice
+    means : np.ndarray
+        Array of locations for the gaussians. Each value must be less 
+        than uc_size. Default variance is uc_size/10
+        
+    Returns
+    -------
+        Array containg the lattice
+    """
     uc = np.zeros((uc_size,uc_size))
     X,Y = np.meshgrid(np.arange(uc_size),np.arange(uc_size))
     pos = np.empty(X.shape + (2,))
@@ -89,12 +165,19 @@ def gaussian_lattice(n_atoms,uc_size,n_tiles, means):
     for i in range(n_atoms):
         mvn = multivariate_normal(mean=means[i],cov = uc_size/10.)
         uc = uc + mvn.pdf(pos)
-    #return uc
     return np.tile(uc,(n_tiles,n_tiles))
 
 
-#An example gaussian lattice with a unit cell containing 10 atoms in a ring
 def rings():
+    """
+    Example Gaussian lattice with a unit cell containing a ring of 10 
+    atoms
+
+    Returns
+    -------
+    np.ndarray
+        array containing the density at each point in the lattice
+    """
     th = np.linspace(0,2*np.pi,11)
     circ_mu = np.array([[50+20*np.cos(t),50+20*np.sin(t)] for t in th[:-1]])
     return gaussian_lattice(10,100,20, circ_mu)
