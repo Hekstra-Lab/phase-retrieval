2e819c74b2519701f710a6fb3e176135aee45a37
ianhi <ianhuntisaak@gmail.com>
2018-03-09 15:52:26 -0500

    add real space compare func

--- phase_mixing_utils.py
+++ phase_mixing_utils.py
@@ -1,7 +1,7 @@
 """
 phase_mixing_utils.py
 
-Utilities for messing around with phase retrieval algorithms and 
+Utilities for messing around with phase retrieval algorithms and
 visualizing the phase problem
 """
 
@@ -11,6 +11,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from cmocean.cm import phase as phase_cm
+from skimage.feature import register_translation
 
 def get_mag(FT_arr):
     """
@@ -46,7 +47,7 @@ def get_phase(FT_arr):
 
 def mix_FT(mag_arr, phase_arr):
     """
-    Performs the Fourier transforms and then mixes the provided phases 
+    Performs the Fourier transforms and then mixes the provided phases
     and magnitudes.
 
     Parameters
@@ -65,9 +66,9 @@ def mix_FT(mag_arr, phase_arr):
 
 def mix_FT_arr(mag_arr, phase_arr):
     """
-    Mixes two Fourier transform arrays by taking the magnitude from one 
+    Mixes two Fourier transform arrays by taking the magnitude from one
     and the phase from the other. Arrays must be the same shape.
-    
+
     Parameters
     ----------
     mag_arr : np.ndarray
@@ -94,7 +95,7 @@ def phase_intensity_plot(arr, ax=None, cb=True, min_alpha=0.0, cmap='ocean'):
     ----------
     arr : np.ndarray
         An array of complex numbers. This should be fftshifted already.
-    ax : matplotlib axis object 
+    ax : matplotlib axis object
         If provided, plot is drawn to axis object
     cb : boolean
         Show colorbar corresponding to phases (Default is True).
@@ -102,12 +103,12 @@ def phase_intensity_plot(arr, ax=None, cb=True, min_alpha=0.0, cmap='ocean'):
         Map the magnitude of intensities to alphas: [min_alpha,1]
         (Default is 0.0).
     cmap : matplotlib colormap
-        Colormap for phases -- string will be used if provided. 
+        Colormap for phases -- string will be used if provided.
         (Default is cmocean phase cm -- hsv is a good alternative).
-    
+
     Returns
     -------
-        Phase plot following Kevin Cowtan's Book of Fourier convention. 
+        Phase plot following Kevin Cowtan's Book of Fourier convention.
     """
 
     r = get_mag(arr)
@@ -121,7 +122,7 @@ def phase_intensity_plot(arr, ax=None, cb=True, min_alpha=0.0, cmap='ocean'):
 
     # Move minimal alpha value to min_alpha
     disp_arr[:,:,-1] += (1-disp_arr[:,:,-1])*min_alpha
-    
+
     if ax is None:
         fig, ax = plt.subplots(figsize=(10,10))
     elif ax is not None and cb:
@@ -137,16 +138,16 @@ def phase_intensity_plot(arr, ax=None, cb=True, min_alpha=0.0, cmap='ocean'):
         cbar.ax.set_yticklabels(labels, fontsize=20)
     else:
         cax = ax.imshow(np.flipud(disp_arr), cmap=cmap)
-        
+
     if ax is None:
-	plt.show()
+        plt.show()
 
 def gaussian_lattice(n_atoms, uc_size, n_tiles, means):
     """
     Create a square lattice composed of unit cells each containg n_atoms
     gaussians of unit variance.
 
-    Lattice dimensions are (uc_size*n_tiles)x(uc_size*n_tiles) 
+    Lattice dimensions are (uc_size*n_tiles)x(uc_size*n_tiles)
 
     Parameters
     ----------
@@ -157,9 +158,9 @@ def gaussian_lattice(n_atoms, uc_size, n_tiles, means):
     n_tiles : float or int
         Number of unit cells to tile to generate lattice
     means : np.ndarray
-        Array of locations for the gaussians. Each value must be less 
+        Array of locations for the gaussians. Each value must be less
         than uc_size. Default variance is uc_size/10
-        
+
     Returns
     -------
         Array containg the lattice
@@ -176,7 +177,7 @@ def gaussian_lattice(n_atoms, uc_size, n_tiles, means):
 
 def rings():
     """
-    Example Gaussian lattice with a unit cell containing a ring of 10 
+    Example Gaussian lattice with a unit cell containing a ring of 10
     atoms
 
     Returns
@@ -187,3 +188,33 @@ def rings():
     th = np.linspace(0,2*np.pi,11)
     circ_mu = np.array([[50+20*np.cos(t),50+20*np.sin(t)] for t in th[:-1]])
     return gaussian_lattice(10,100,20, circ_mu)
+def align_and_plot(pr, true_im):
+    """
+    Determines the proper rotation and translation for matching the reconstructed real space image to the true one
+
+    inputs
+    --------
+    pr : a PhaseRetrieval object that has run some reconstruction
+    true_im : ndarry of the original real space image
+    """
+    errs = np.zeros(4)
+    for i in range(4):
+        shift, errs[i], blarg = register_translation(true_im, np.rot90(pr.real_space_guess,k=i))
+    n_rot = np.argmin(errs)
+    shift, error, blargh= register_translation(true_im,np.rot90(pr.real_space_guess,k=n_rot))
+
+
+    fixed = np.roll(np.rot90(pr.real_space_guess,k=n_rot),shift.astype(np.int),axis=(0,1))
+    plt.imshow(fixed)
+    plt.show()
+    plt.title('orig')
+    plt.imshow(true_im)
+    plt.show()
+
+    error = np.zeros(pr.rs_track.shape[0])
+    for i,im in enumerate(pr.rs_track):
+        shift, error[i], diffphase = register_translation(true_im, np.rot90(im,k=n_rot))
+
+    plt.plot(error)
+    plt.ylabel('error')
+    plt.show()
